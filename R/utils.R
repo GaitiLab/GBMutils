2b8b80aed22cc6a4c5731abaafcd3a50db97ff43
joan-yanqiong <JoanKant@uhn.ca>
2024-04-09 17:38:06 -0400

    add function for merging a list of dataframes (> 2)

--- R/utils.R
+++ R/utils.R
@@ -121,3 +121,14 @@ generate_pairs <- function(v, collapse = "__", remove_self = TRUE, return_undire
         return(apply(pairs, 1, paste0, collapse = collapse))
     }
 }
+
+#' @title Merge list of dataframes 
+#' @param list_of_dfs list of dataframes for merging
+#' @param by merge on key (Default = i)
+#' @param all.x merge on x (left dataframe)
+merge_dfs <- function(list_of_dfs, by, all.x = TRUE) {
+    Reduce(
+        function(dtf1, dtf2) merge(dtf1, dtf2, by = by, all.x = all.x),
+        list_of_dfs
+    )
+}
