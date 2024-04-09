e85343fde845f6fff8dc93f0494d602f72a938dc
joan-yanqiong <JoanKant@uhn.ca>
2024-04-09 17:39:38 -0400

    export function

--- R/utils.R
+++ R/utils.R
@@ -126,6 +126,7 @@ generate_pairs <- function(v, collapse = "__", remove_self = TRUE, return_undire
 #' @param list_of_dfs list of dataframes for merging
 #' @param by merge on key (Default = i)
 #' @param all.x merge on x (left dataframe)
+#' @export
 merge_dfs <- function(list_of_dfs, by, all.x = TRUE) {
     Reduce(
         function(dtf1, dtf2) merge(dtf1, dtf2, by = by, all.x = all.x),
