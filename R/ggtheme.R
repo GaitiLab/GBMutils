5c883088d33902e68875353e0a9da75fa4decb2a
joan-yanqiong <JoanKant@uhn.ca>
2024-04-26 12:13:38 -0400

    replace element_text with element_markdown for ggplot subscript and superscript

--- R/ggtheme.R
+++ R/ggtheme.R
@@ -6,25 +6,25 @@
 #' @import ggplot2
 #' @import ggthemes
 #' @import grid
+#' @import ggtext
 
 GBM_theme <- function(base_size = 14, base_family = "Helvetica") {
     (theme_foundation(base_size = base_size, base_family = base_family)
     + theme(
-            plot.title = element_text(
+            plot.title = element_markdown(
                 face = "bold",
                 size = rel(1.2), hjust = 0.5
             ),
-            text = element_text(),
+            text = element_markdown(),
             panel.background = element_rect(colour = NA),
             plot.background = element_rect(colour = NA),
             panel.border = element_rect(colour = NA),
-            axis.title = element_text(face = "bold", size = rel(1.1)),
-            axis.title.y = element_text(angle = 90, vjust = 2),
-            axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(size = rel(1)),
+            axis.title = element_markdown(face = "bold", size = rel(1.1)),
+            axis.title.y = element_markdown(angle = 90, vjust = 2),
+            axis.title.x = element_markdown(vjust = -0.2),
+            axis.text = element_markdown(size = rel(1)),
             axis.line = element_line(colour = "black"),
             axis.ticks = element_line(),
-            # panel.grid.major = element_line(colour = "#f0f0f0"),
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
             legend.key = element_rect(colour = NA),
@@ -32,9 +32,9 @@ GBM_theme <- function(base_size = 14, base_family = "Helvetica") {
             legend.direction = "horizontal",
             legend.key.size = unit(0.2, "cm"),
             legend.spacing = unit(0, "cm"),
-            legend.title = element_text(face = "plain"),
+            legend.title = element_markdown(face = "plain"),
             plot.margin = unit(c(10, 5, 5, 5), "mm"),
             strip.background = element_rect(colour = "#ffffff", fill = "#ffffff"),
-            strip.text = element_text(face = "bold", size = rel(1.1))
+            strip.text = element_markdown(face = "bold", size = rel(1.1))
         ))
 }
