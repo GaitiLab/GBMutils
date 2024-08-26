c3467c1f0db1ec7208d6731b946cc140fced7009
joan-yanqiong <Joan.Kant@uhn.ca>
2024-08-26 19:31:04 -0400

    fix signature

--- R/load_data.R
+++ R/load_data.R
@@ -1,20 +1,20 @@
 #' @title Load genelist of invasive signature
 #' @description Load invasive signature derived from comparing PT OPC/NPC1 to Tumour bulk OPC/NPC1 cells.
-#' @param type 'high' or 'low' (default = TRUE)
+#' @param type choose either 'high' or 'low' (default = 'high')
 #' @return A list containing two vectors. Invasive up genes at index 1. Invasive down genes at index 2
-#' @importFrom dplyr %>% filter
+#' @importFrom dplyr %>%
 #' @export
 load_invasive_signature <- function(type = "high") {
     degs_up <- degs_signature %>%
-        filter(log2FoldChange > 1 & padj < 0.05)
+        dplyr::filter(log2FoldChange > 1 & padj < 0.05) %>%
+        dplyr::pull(gene)
     degs_dn <- degs_signature %>%
-        filter(log2FoldChange < -1 & padj < 0.05)
+        dplyr::filter(log2FoldChange < -1 & padj < 0.05) %>%
+        dplyr::pull(gene)
     if (type == "high") {
-    return(degs_up)
+        return(degs_up)
     } else if (type == "low") {
         return(degs_dn)
-    } else {
-        warning("Choose type = 'high' or type = 'low'")
     }
 }
 
